57ad802507ced586f78f6d5c7f725813f70d4e2f
maciejleszcz95 <maciejleszczynski@icloud.com>
2024-07-02 12:30:51 +0200

    Basic stack implementation.

---
+++ MotoApp/MotoApp/BasicStack.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotoApp
+{
+    internal class BasicStack
+    {
+        private readonly double[] _items;
+        
+        private int _currentIndex = -1;
+
+        public BasicStack() => _items = new double[10];
+
+        public int Count => _currentIndex + 1;
+
+        public void Push(double item) => _items[++_currentIndex] = item;
+
+        public double Pop() => _items[_currentIndex--];
+    }
+}
--- MotoApp/MotoApp/Program.cs
+++ MotoApp/MotoApp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MotoApp;
+
+var stack = new BasicStack();
+
+stack.Push(4.5);
+stack.Push(43);
+stack.Push(333.6);
+
+double sum = 0.0;
+
+while (stack.Count > 0)
+{
+    double item = stack.Pop();
+    Console.WriteLine($"Item: {item}");
+    sum += item;
+}
+
+Console.WriteLine($"Sum {sum}");
